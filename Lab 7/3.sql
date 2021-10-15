0aab2929d21900d62b09cb6af473fff4b1c359c9
Muhammed Gazi <80915646+MuhammedGazi05@users.noreply.github.com>
2021-10-16 00:09:03 +0300

    Delete Lab 7 directory

--- Lab 7/3.sql
+++
@@ -1,4 +0,0 @@
--- In 3.sql, write a SQL query to list the names of the top 5 largest songs,
--- in descending orde rof length.
-
-SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
--- Lab 7/7.sql
+++
@@ -1,3 +0,0 @@
--- In 7.sql, write a SQL query that returns the average energy of songs that are by Drake.
-
-SELECT avg(energy) FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name == "Drake")
